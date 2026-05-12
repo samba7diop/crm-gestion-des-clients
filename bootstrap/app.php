d8bebf2dca13efc62f275595527bfcd94f5edea2
sowsalim01 <sowsalim01@gmail.com>
2026-05-12 20:43:05 +0000

    Eliminer l'avertisseent

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,14 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
-        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_ALL);
+        $middleware->trustProxies(headers: [
+            Request::HEADER_FORWARDED,
+            Request::HEADER_X_FORWARDED_FOR,
+            Request::HEADER_X_FORWARDED_HOST,
+            Request::HEADER_X_FORWARDED_PORT,
+            Request::HEADER_X_FORWARDED_PROTO,
+            Request::HEADER_X_FORWARDED_AWS_ELB,
+        ]);
 
         $middleware->alias([
             'role' => EnsureRole::class,
